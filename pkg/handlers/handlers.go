3d8669d0ff03c2188fd16f0131f2382ab60f5798
CSantana23 <cesarsantana475@gmail.com>
2021-10-22 13:01:42 -0400

    creating template caching and template sharing option for golang

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,15 +1,42 @@
 package handlers
 
 import (
+	"myApp/pkg/config"
+	"myApp/pkg/models"
 	"myApp/pkg/render"
 	"net/http"
 )
 
-// Home functions that handle web request need to handle two parameters a responseWriter and request
-func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+//Repo the repository used by the handlers
+var Repo *Repository
+
+//Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
 }
 
-func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+//NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+//NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home functions that handle web request need to handle two parameters a responseWriter and request
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello Again"
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+
 }
