840f182bfa3565307aae5bc375faf5adb089bcfc
CSantana23 <cesarsantana475@gmail.com>
2021-10-22 09:46:20 -0400

    creating goLang HTML template files and wiring them up to work

--- main.go
+++ main.go
@@ -3,17 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
 
 // Home functions that handle web request need to handle two parameters a responseWriter and request
 func Home(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "home.page.tmpl")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
 
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parseTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+	}
 }
 
 func main() {
