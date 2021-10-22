03148959bd4ddac15202840379f7f7e4c2fe6594
CSantana23 <cesarsantana475@gmail.com>
2021-10-22 10:11:19 -0400

    adding bootstrap css theme and refactoring the main program

--- main.go
+++ main.go
@@ -1,29 +1,14 @@
+//to run just the main file use go run main.go
+// to run all the go file use go run *.go
+
 package main
 
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
-
-// Home functions that handle web request need to handle two parameters a responseWriter and request
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parseTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-	}
-}
 
 func main() {
 	http.HandleFunc("/", Home)
