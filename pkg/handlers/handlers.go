4e966bd4c658d4f9f2f556bcc7744a93b5af4851
CSantana23 <cesarsantana475@gmail.com>
2021-10-28 16:17:47 -0400

    testing session and getting IP address from user

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -29,12 +29,18 @@ func NewHandlers(r *Repository) {
 
 // Home functions that handle web request need to handle two parameters a responseWriter and request
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello Again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
